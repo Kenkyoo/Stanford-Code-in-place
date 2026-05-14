b659a592f40b3daae655539959943f4d9cf8e951
Franco <frfantusatti@protonmail.com>
2026-05-14 20:55:54 -0300

    finish week 5

---
+++ Week5-Graphics/Pyramid/main.py
@@ -0,0 +1,30 @@
+from graphics import Canvas
+import random
+
+CANVAS_WIDTH = 600      # Width of drawing canvas in pixels
+CANVAS_HEIGHT = 300     # Height of drawing canvas in pixels
+
+BRICK_WIDTH	= 30        # The width of each brick in pixels
+BRICK_HEIGHT = 12       # The height of each brick in pixels
+BRICKS_IN_BASE = 14     # The number of bricks in the base
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    # TODO, your code here
+    for row in range(BRICKS_IN_BASE):
+       bricks_in_row = BRICKS_IN_BASE - row
+       row_width = bricks_in_row * BRICK_WIDTH
+       start_x = (CANVAS_WIDTH - row_width) / 2
+       top_y = CANVAS_HEIGHT - BRICK_HEIGHT * (row + 1)
+       for brick in range(bricks_in_row):
+           x = start_x + brick * BRICK_WIDTH
+           canvas.create_rectangle(
+                x,
+                top_y,
+                x + BRICK_WIDTH,
+                top_y + BRICK_HEIGHT,
+                "yellow",
+                "black"
+            )
+if __name__ == '__main__':
+    main()
