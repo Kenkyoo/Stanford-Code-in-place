9ce0e27b0b91c3f95edd4a5259388a7591170386
Franco <frfantusatti@protonmail.com>
2026-05-20 04:44:02 -0300

    add challenges for week 6

---
+++ Week6-Data/Compute-Average/main.py
@@ -0,0 +1,27 @@
+import statistics
+
+def main():
+    number_list = load_numbers_from_file("numbers.txt")
+    # TODO: your code here
+
+
+def load_numbers_from_file(filepath):
+    """
+    Loads numbers from a file into a list and returns it.
+    We assume the file to have one number per line.
+    Returns a list of numbers. You should not modify this
+    function.
+    """
+    numbers = []
+    with open(filepath, 'r') as file_reader:
+        for line in file_reader.readlines():
+            cleaned_line = line.strip()
+            if cleaned_line != '':
+                numbers.append(float(cleaned_line))
+    
+    a = statistics.mean(numbers)
+    print(f"Average: {a}")
+
+
+if __name__ == '__main__':
+    main()
